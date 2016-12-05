194ffb1469f7346002043a87d25de24b7135d0f0
Seliverstov Maksim <maksim.v.seliverstov@yandex.ru>
2016-12-06 00:54:07 +0300

    en: Added functions to parse following slp messages: ru: Добавлены функции для парсинга следующих сообщений slp протокола: - service request - service reply - service registration - service deregister - service acknowledge - attribute request - attribute reply

---
+++ pyslp/parse.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+
+def convert_to_int(data):
+    s = '0b'
+    for b in data:
+        s += str(bin(b))[2:]
+    return int(s, 2)
+
+
+def _parse(data, count):
+    p = 0
+    result = list()
+    for _ in range(count):
+        length = convert_to_int(data[p:p + 2])
+        p += 2
+        value = data[p:p + length]
+        p += length
+        result.append(
+            dict(
+                value=value,
+                length=length
+            )
+        )
+
+    return [info['value'] for info in result], 7 + sum([info['length'] for info in result])
+
+
+def parse_header(data):
+    language_tag_length = convert_to_int(data[12:14])
+    header_length = 14 + language_tag_length
+    return dict(
+        version=data[0],
+        function_id=data[1],
+        length=convert_to_int(data[2:5]),
+        xid=convert_to_int(data[10:12]),
+        language_tag_length=language_tag_length,
+        language_tag=data[14:header_length].decode()
+    ), header_length
+
+
+def parse_url_entry(data):
+    url_length = convert_to_int(data[3:5])
+    auth_length = data[5 + url_length]
+    length = 6 + url_length + auth_length
+    return dict(
+        lifetime=convert_to_int(data[1:3]),
+        url=data[5:5 + url_length].decode()
+    ), length
+
+
+def parse_registration(data):
+    header, header_length = parse_header(data)
+    url_entry, url_entries_length = parse_url_entry(data[header_length:])
+
+    result, length = _parse(data[header_length + url_entries_length:], 3)
+
+    return header, url_entry, dict(
+        service_type=result[0].decode(),
+        scope_list=result[1].decode(),
+        attr_list=result[2].decode()
+    )
+
+
+def parse_request(data):
+    header, header_length = parse_header(data)
+    result, length = _parse(data[header_length:], 5)
+    return header, dict(
+        service_type=result[1].decode(),
+        scope_list=result[2].decode()
+    )
+
+
+def parse_reply(data):
+    header, header_length = parse_header(data)
+    p = header_length
+    error_code = convert_to_int(data[p:p + 2])
+    p += 2
+    url_count = convert_to_int(data[p:p + 2])
+    p += 2
+    url_entries = list()
+    for _ in range(url_count):
+        url_entry, url_entry_length = parse_url_entry(data[p:])
+        url_entries.append(url_entry)
+        p += url_entry_length
+    return header, error_code, url_entries
+
+
+def parse_acknowledge(data):
+    header, header_length = parse_header(data)
+    error_code = convert_to_int(data[header_length:header_length + 2])
+    return header, error_code
+
+
+def parse_attr_request(data):
+    header, header_length = parse_header(data)
+    result, length = _parse(data[header_length:], 5)
+    return header, dict(
+        url=result[1].decode(),
+        scope_list=result[2].decode()
+    )
+
+
+def parse_attr_reply(data):
+    header, header_length = parse_header(data)
+    p = header_length
+    error_code = convert_to_int(data[p:p + 2])
+    p += 2
+    attr_list_length = convert_to_int(data[p:p + 2])
+    p += 2
+    attr_list = data[p:p + attr_list_length].decode()
+    p += attr_list_length
+    return header, error_code, attr_list
+
+
+def parse_deregistration(data):
+    header, header_length = parse_header(data)
+    p = header_length
+    scope_list_length = convert_to_int(data[p:p + 2])
+    p += 2
+    scope_list = data[p:p + scope_list_length].decode()
+    p += scope_list_length
+    url_entry, url_entry_length = parse_url_entry(data[p:])
+    p += url_entry_length
+    tag_list_length = convert_to_int(data[p:p + 2])
+    p += 2
+    tag_list = data[p:]
+    return header, url_entry, scope_list
---
+++ pyslp/tests/test_parse.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from pyslp import parse
+
+
+class TestMultiCast(unittest.TestCase):
+
+    def test_parse_registration(self):
+        data = b"\x02\x03\x00\x00R@\x00\x00\x00\x00UI\x00\x02en\x00\x00\x0f\x00\x17service:test://test.com\x00\x00\x0cservice:test\x00\x05anapa\x00\r(attr='test')\x00"
+        header, url_entries, msg = parse.parse_registration(data)
+        self.assertDictEqual(
+            header, dict(
+                version=2,
+                function_id=3,
+                xid=10953,
+                length=82,
+                language_tag_length=2,
+                language_tag='en'
+            )
+        )
+        self.assertDictEqual(
+            url_entries, dict(
+                lifetime=15,
+                url='service:test://test.com'
+            )
+        )
+        self.assertDictEqual(
+            msg, dict(
+                service_type='service:test',
+                scope_list='anapa',
+                attr_list="(attr='test')"
+            )
+        )
+
+    def test_parse_request(self):
+        data = b'\x02\x01\x00\x00* \x00\x00\x00\x00&\x0c\x00\x02en\x00\x00\x00\x0cservice:test\x00\x04test\x00\x00\x00\x00'
+        header, msg = parse.parse_request(data)
+        self.assertDictEqual(
+            header, dict(
+                version=2,
+                function_id=1,
+                xid=620,
+                length=42,
+                language_tag_length=2,
+                language_tag='en'
+            )
+        )
+        self.assertDictEqual(
+            msg, dict(
+                service_type='service:test',
+                scope_list='test'
+            )
+        )
+
+    def test_parse_reply(self):
+        data = b'\x02\x02\x00\x001\x00\x00\x00\x00\x00?\xca\x00\x02en\x00\x00\x00\x01\x00\xff\xff\x00\x17service:test://test.com\x00'
+        header, error_code, url_entries = parse.parse_reply(data)
+        self.assertDictEqual(
+            header, dict(
+                version=2,
+                function_id=2,
+                xid=16330,
+                length=49,
+                language_tag_length=2,
+                language_tag='en'
+            )
+        )
+        self.assertEqual(error_code, 0)
+        self.assertEqual(len(url_entries), 1)
+        self.assertDictEqual(
+            url_entries[0], dict(
+                lifetime=65535,
+                url='service:test://test.com'
+            )
+        )
+
+    def test_parse_attr_request(self):
+        data = b'\x02\x06\x00\x008 \x00\x00\x00\x00\x1e\xf2\x00\x02en\x00\x00\x00\x17service:test://test.com\x00\x07DEFAULT\x00\x00\x00\x00'
+        header, msg = parse.parse_attr_request(data)
+        self.assertDictEqual(
+            header, dict(
+                version=2,
+                function_id=6,
+                xid=7922,
+                length=56,
+                language_tag_length=2,
+                language_tag='en'
+            )
+        )
+        self.assertDictEqual(
+            msg, dict(
+                url='service:test://test.com',
+                scope_list='DEFAULT'
+            )
+        )
+
+    def test_parse_attr_reply(self):
+        data = b'\x02\x07\x00\x00\x1d\x00\x00\x00\x00\x00\x96\x7f\x00\x02en\x00\x00\x00\x08(attr=1)\x00'
+        header, error_code, attr_list = parse.parse_attr_reply(data)
+        self.assertDictEqual(
+            header, dict(
+                version=2,
+                function_id=7,
+                xid=19327,
+                length=29,
+                language_tag_length=2,
+                language_tag='en'
+            )
+        )
+        self.assertEqual(error_code, 0)
+        self.assertEqual(attr_list, '(attr=1)')
+
+    def test_parse_deregistration(self):
+        data = b'\x02\x04\x00\x008\x00\x00\x00\x00\x00>\x0c\x00\x02en\x00\x07DEFAULT\x00\x00\x00\x00\x17service:test://test.com\x00\x00\x00'
+        header, url_entry, scope_list = parse.parse_deregistration(data)
+        self.assertDictEqual(
+            header, dict(
+                version=2,
+                function_id=4,
+                xid=1004,
+                length=56,
+                language_tag_length=2,
+                language_tag='en'
+            )
+        )
+        self.assertDictEqual(
+            url_entry, dict(
+                url='service:test://test.com',
+                lifetime=0
+            )
+        )
+        self.assertEqual(scope_list, 'DEFAULT')
